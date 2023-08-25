253db905fc41adc9d4c526cff9afe0f8ce0a73b2
Musa UYUMAZ <musa.uyumaz73@gmail.com>
2023-08-25 20:50:03 +0300

    Remove ignored files

---
+++ 2-Asp.Net Core Dependency Injection(DI)/IOC/IOC.Web/Models/DateService.cs
@@ -0,0 +1,15 @@
+﻿namespace IOC.Web.Models
+{
+    public class DateService : ISingletonDateService, IScopedDateService, ITransientDateService
+    {
+        private readonly ILogger<DateService> _logger;
+
+        public DateService(ILogger<DateService> logger)
+        {
+            _logger = logger;
+            _logger.LogWarning("DateService constructor'ına girdi.");
+        }
+
+        public DateTime GetDateTime { get; } = DateTime.Now;
+    }
+}
---
+++ 2-Asp.Net Core Dependency Injection(DI)/IOC/IOC.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IOC.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
